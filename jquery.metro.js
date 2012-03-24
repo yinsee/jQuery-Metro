d0e3b4bba288674fd49847c8f162e8bbfdc6720d
Daddycat Tan <yinsee@wsatp.com>
2012-03-25 01:24:43 +0800

    add auto spacing

--- jquery.metro.js
+++ jquery.metro.js
@@ -1,3 +1,6 @@
+/*
+usage: $('#metro').metro();
+*/
 (function($){
 	// This function adds noise to the background-image attribute of a given element
 
@@ -6,6 +9,8 @@
 		var no_of_rows = parseInt(options.containerHeight / options.boxSize);
 		if (no_of_rows < 1) on_of_rows = 1;
 		var counters = [];
+		var lastSpacing = 0;
+
 //		console.log('rows:',no_of_rows);
 		for(var i=0;i<no_of_rows;i++) { counters[i] = 0; } // init the row tracker
 
@@ -29,11 +34,34 @@
 				top: options.boxSize * shortestrow
 			};
 			counters[shortestrow] += $(this).outerWidth(true);
+
 //			console.log('insert at', shortestrow, 'move item:', style, ' row size now is:', counters[shortestrow]);
 			$(this).addClass('metro').css(style);
+			
+			// if all rows aligned, space them
+			var rows_same_length = true;
+			for(var i=1;i<no_of_rows;i++) 
+			{
+				if (counters[i-1] != counters[i])
+				{
+					rows_same_length = false;
+				}
+			}
+			if (rows_same_length && counters[0]-lastSpacing >= options.spaceEvery*options.boxSize) 
+			{
+//				console.log('row same lenght!');
+				lastSpacing = counters[0];
+				for(var i=0;i<no_of_rows;i++) 
+				{
+					counters[i] += options.spaceSize;	
+				}
+			}
+				
 		});
 		
-		$(obj).css( { position: 'relative', height: options.boxSize*no_of_rows });
+		var maxWidth = counters[0];
+		for(var i=1;i<no_of_rows;i++) { if (counters[i] > maxWidth) maxWidth = counters[i]; }
+		$(obj).css( { position: 'relative', height: options.boxSize*no_of_rows, width: maxWidth });
 	};
 	
 	$.fn.metro = function(options) {
@@ -46,6 +74,8 @@
 	
 	$.fn.metro.defaults = {
 		boxSize: 150,
-		containerHeight: $(window).height()
+		containerHeight: $(window).height(),
+		spaceEvery: 4,
+		spaceSize: 100
 	};
 })(jQuery);
